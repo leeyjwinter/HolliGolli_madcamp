c75e1d7f49a7e44bfd29a55c9a97bf6fb6dc82b0
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2022-07-23 17:06:00 +0900

    add package

--- parcel/app.js
+++ parcel/app.js
@@ -3,14 +3,38 @@ import * as THREE from "three";
 // import Stats from 'three/examples/jsm/libs/stats.module.js';
 
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 // let stats;
 let camera, scene, renderer;
 
 let group;
+let group2;
+let group3;
+let group4;
+let group1On = true; //right top
+let group2On = false; //right bottom
+let group3On = false; //left bottom
+let group4On = false; //left top
 
 init();
-animate();
+if (group1On) {
+  animate();
+  console.log("group1 animated");
+}
+if (group2On) {
+  animate2();
+  console.log("group2 animated");
+}
+if (group3On) {
+  animate3();
+  console.log("group3 animated");
+}
+if (group4On) {
+  animate4();
+  console.log("group4 animated");
+}
 
 function init() {
   const container = document.createElement("div");
@@ -55,9 +79,9 @@ function init() {
   scene.add(light);
 
   // scene.add( new DirectionalLightHelper( light ) );
-  scene.add(new THREE.CameraHelper(light.shadow.camera));
+  //   scene.add(new THREE.CameraHelper(light.shadow.camera));
 
-  // group
+  // group1
 
   group = new THREE.Group();
   scene.add(group);
@@ -73,18 +97,87 @@ function init() {
     sphere.position.x = 5;
     sphere.position.z = 0;
 
-    // sphere.position.normalize();
-    // sphere.position.multiplyScalar( Math.random() * 2 + 1 );
     sphere.castShadow = true;
     sphere.receiveShadow = true;
     sphere.userData.phase = Math.random() * Math.PI;
-    group.add(sphere);
+    if (group1On) group.add(sphere);
+  }
+
+  //group2
+
+  group2 = new THREE.Group();
+  scene.add(group2);
+
+  const geometry2 = new THREE.SphereGeometry(0.3, 20, 20);
+
+  for (let i = 0; i < 5; i++) {
+    const material = new THREE.MeshPhongMaterial({
+      color: Math.random() * 0xffffff,
+    });
+
+    const sphere = new THREE.Mesh(geometry2, material);
+    sphere.position.x = 5;
+    sphere.position.z = 0;
+
+    sphere.castShadow = true;
+    sphere.receiveShadow = true;
+    sphere.userData.phase = Math.random() * Math.PI;
+    if (group2On) group2.add(sphere);
+  }
+
+  //group3
+
+  group3 = new THREE.Group();
+  scene.add(group3);
+
+  const geometry3 = new THREE.SphereGeometry(0.3, 20, 20);
+
+  for (let i = 0; i < 5; i++) {
+    const material = new THREE.MeshPhongMaterial({
+      color: Math.random() * 0xffffff,
+    });
+
+    const sphere = new THREE.Mesh(geometry3, material);
+    sphere.position.x = 5;
+    sphere.position.z = 0;
+
+    sphere.castShadow = true;
+    sphere.receiveShadow = true;
+    sphere.userData.phase = Math.random() * Math.PI;
+    if (group3On) group3.add(sphere);
+  }
+
+  //group4
+
+  group4 = new THREE.Group();
+  scene.add(group4);
+
+  const geometry4 = new THREE.SphereGeometry(0.3, 20, 20);
+
+  for (let i = 0; i < 5; i++) {
+    const material = new THREE.MeshPhongMaterial({
+      color: Math.random() * 0xffffff,
+    });
+
+    const sphere = new THREE.Mesh(geometry4, material);
+    sphere.position.x = 5;
+    sphere.position.z = 0;
+
+    sphere.castShadow = true;
+    sphere.receiveShadow = true;
+    sphere.userData.phase = Math.random() * Math.PI;
+    if (group4On) group4.add(sphere);
   }
 
   // ground
 
   const groundMaterial = new THREE.MeshPhongMaterial({
     color: 0x404040,
+    specular: 0x111111,
+  });
+
+  const bellMaterial = new THREE.MeshPhongMaterial({
+    color: 0xffffff,
     specular: 0x111111,
   });
 
@@ -96,13 +189,33 @@ function init() {
   ground.receiveShadow = true;
   scene.add(ground);
 
-  // column
-
-  const column = new THREE.Mesh(new THREE.BoxGeometry(1, 4, 1), groundMaterial);
-  column.position.y = 2;
+  const column = new THREE.Mesh(
+    new THREE.CylinderGeometry(0.2, 0.2, 2.8, 32),
+    bellMaterial
+  );
+  column.position.y = 0;
   column.castShadow = true;
   column.receiveShadow = true;
   scene.add(column);
+
+  //bell
+  const loader = new GLTFLoader();
+  //   const dracoLoader = new DRACOLoader();
+  //   dracoLoader.setDecoderPath( '/examples/js/libs/draco/' );
+  //   loader.setDRACOLoader( dracoLoader );
+  loader.load(
+    "https://cdn.jsdelivr.net/gh/mrdoob/three.js@master/examples/models/gltf/AnimatedMorphSphere/glTF/AnimatedMorphSphere.gltf",
+    function (gltf) {
+      scene.add(gltf.scene);
+    }
+  );
+  //   },
+
+  //   undefined, function ( error ) {
+
+  //    console.error( error );
+
+  // } );
 
   // overwrite shadowmap code
 
@@ -141,13 +254,6 @@ function init() {
   controls.target.set(0, 2.5, 0);
   controls.update();
 
-  // performance monitor
-
-  // stats = new Stats();
-  // container.appendChild( stats.dom );
-
-  //
-
   window.addEventListener("resize", onWindowResize);
 }
 
@@ -160,31 +266,90 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-//
-
 function animate() {
   const time = performance.now() / 1000;
-  console.log(time);
 
   group.traverse(function (child) {
     if ("phase" in child.userData) {
       child.position.y =
         Math.abs(Math.sin(time + child.userData.phase)) * 4 + 0.3;
-      console.log(child.userData.phase);
-      child.position.z = time-8;
-	  if(time>8){
-		child.position.z=0;
-	  }
-	  child.position.x = time-8;
-	  if(time>8){
-		child.position.x=0;
-	  }
+      child.position.z = time - 8;
+      if (time > 8) {
+        child.position.z = (time % 3) * 0.01;
+      }
+      child.position.x = time - 8;
+      if (time > 8) {
+        child.position.x = (time % 3) * 0.01;
+      }
     }
   });
 
   renderer.render(scene, camera);
-
-  // stats.update();
-
   requestAnimationFrame(animate);
+}
+
+function animate2() {
+  const time = performance.now() / 1000;
+
+  group2.traverse(function (child) {
+    if ("phase" in child.userData) {
+      child.position.y =
+        Math.abs(Math.sin(time + child.userData.phase)) * 4 + 0.3;
+      child.position.z = time - 8;
+      if (time > 8) {
+        child.position.z = (time % 3) * 0.01;
+      }
+      child.position.x = -time + 8;
+      if (time > 8) {
+        child.position.x = (time % 3) * 0.01;
+      }
+    }
+  });
+
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate2);
+}
+
+function animate3() {
+  const time = performance.now() / 1000;
+
+  group3.traverse(function (child) {
+    if ("phase" in child.userData) {
+      child.position.y =
+        Math.abs(Math.sin(time + child.userData.phase)) * 4 + 0.3;
+      child.position.z = -time + 8;
+      if (time > 8) {
+        child.position.z = (time % 3) * 0.01;
+      }
+      child.position.x = -time + 8;
+      if (time > 8) {
+        child.position.x = (time % 3) * 0.01;
+      }
+    }
+  });
+
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate3);
+}
+
+function animate4() {
+  const time = performance.now() / 1000;
+
+  group4.traverse(function (child) {
+    if ("phase" in child.userData) {
+      child.position.y =
+        Math.abs(Math.sin(time + child.userData.phase)) * 4 + 0.3;
+      child.position.z = -time + 8;
+      if (time > 8) {
+        child.position.z = (time % 3) * 0.01;
+      }
+      child.position.x = time - 8;
+      if (time > 8) {
+        child.position.x = (time % 3) * 0.01;
+      }
+    }
+  });
+
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate4);
 }
